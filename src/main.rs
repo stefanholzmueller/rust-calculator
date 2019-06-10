776c591318ca423822d50741acd813ec88cb49a8
Stefan Holzmüller <stefan.holzmueller@ligadigital.com>
2019-06-10 13:13:54 +0200

    parses an integer number

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use combine::many1;
-use combine::Parser;
-use combine::parser::byte::digit;
-
+use combine::parser::char::digit;
+use combine::parser::Parser;
 use num_bigint::BigInt;
 use num_rational::Ratio;
-
+use combine::parser::repeat::Many1;
 
 fn main() {
     let args = std::env::args().skip(1);
     let input = args.collect::<Vec<String>>().join(" ");
-    let expr = parse(input);
-    let result = eval(expr);
-    println!("= {}", result);
+    let expr_result = parse(input);
+    let calc_result = expr_result.map(eval);
+    match calc_result {
+        Ok(result) => println!("{:?}", result),
+        Err(err) => println!("error: {}", err)
+    }
 }
 
 type Num = Ratio<BigInt>;
@@ -20,15 +22,19 @@ enum Expr {
     Nat(Num),
 }
 
-fn parse(input: String) -> Expr {
+fn parse(input: String) -> Result<Expr, String> {
+    println!("input = {}", input);
     let slice: &str = &input[..];
-    let parser = many1(digit());
-    let result = parser.easy_parse(slice);
-    match result {
-        Ok((value, _remaining_input)) => println!("{:?}", value),
-        Err(err) => println!("error: {}", err)
-    }
-    Expr::Nat(Ratio::new(BigInt::from(123), BigInt::from(1)))
+    let mut parser: Many1<Vec<_>, _> = many1(digit());
+    let result = parser.easy_parse(slice).map(|r| r.0);
+    let expr = result.map(parse_number);
+    return expr.map_err(|e| e.to_string());
+}
+
+fn parse_number(input: Vec<char>) -> Expr {
+    let str: String = input.into_iter().collect();
+    let bi: BigInt = str.parse().expect("CRAP");
+    return Expr::Nat(Ratio::new(bi, BigInt::from(1)));
 }
 
 fn eval(expr: Expr) -> Ratio<BigInt> {
@@ -36,4 +42,3 @@ fn eval(expr: Expr) -> Ratio<BigInt> {
         Expr::Nat(ratio) => ratio,
     }
 }
-
